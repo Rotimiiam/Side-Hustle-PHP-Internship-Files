91506a2e120fdc79f0fa041e23bf1f2b4dcee8d9
Olorode Durotimi <rotimiiam@gmail.com>
2020-10-16 15:05:43 +0100

    Add files via upload

---
+++ basicsyntax_majortask.php
@@ -0,0 +1,18 @@
+<?php
+//======= first major task.======
+
+/*
+Occupation: Developer
+Gender: Male
+Sidehustle Username: Rotimiiam
+*/
+
+//about me tag
+echo "<h1>About me</h1> </hr>";
+
+//my side hustle internship username
+echo "<h3>Rotimiiam</h3>";
+
+//about myself
+echo "<p>I am a mettaluirgical and materials enineering student in the university of lagos, I am 20 years old and I live in Lagos Nigeria. I'm a intermediate developer and still polishing my skills , I have a few hobbies which are reading novels, watching anime and playing video games.</p>";
+
